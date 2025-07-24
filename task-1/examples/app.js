739c1734e50820b9ce7a5aeb18a01bc83538a75c
Vladislav Chervyakov <chervyakovve@mail.ru>
2025-07-24 12:57:48 +0700

    feat: measure performance

--- task-1/examples/app.js
+++ task-1/examples/app.js
@@ -1,56 +1,47 @@
-const { Worker } = require('worker_threads');
-const { fork } = require('child_process');
-const { performance, PerformanceObserver } = require('perf_hooks');
-const { readFileSync } = require('fs');
+import { Worker } from 'node:worker_threads';
+import { fork } from 'node:child_process';
+import perf_hooks from 'node:perf_hooks';
 
-const file = readFileSync('./file.mp4');
 
-const performanceObserver = new PerformanceObserver((items) => {
-	items.getEntries().forEach((entry) => {
-		console.log(`${entry.name}: ${entry.duration}`);
-	});
+function runWorker() {
+  return new Promise((res) => {
+    performance.mark('worker start');
+    const worker = new Worker('./worker.js', { workerData: { array: [1, 2, 3, 4, 5] } });
+    worker.on('message', (data) => {
+      performance.mark('worker end');
+      performance.measure('worker', 'worker start', 'worker end');
+
+      res(data);
+    });
+    worker.on('error', (err) => console.log(err.message));
+    
+
+  });
+}
+
+function runFork() {
+  return new Promise((res) => {
+    performance.mark('fork start');
+    const forkProcess = fork('./fork.js');
+    forkProcess.send({ array: [1, 2, 3] });
+    forkProcess.on('message', (msg) => {
+      performance.mark('fork end');
+      performance.measure('fork', 'fork start', 'fork end');
+      res(msg);
+    });
+
+  });
+}
+
+const timedWorker = perf_hooks.performance.timerify(runWorker);
+const timedFork = perf_hooks.performance.timerify(runFork);
+
+const performanceObserver = new perf_hooks.PerformanceObserver((items, observer) => {
+  items.getEntries().forEach((entry) => console.log(`${entry.name} `, entry.duration));
+  // observer.disconnect();
 });
 performanceObserver.observe({ entryTypes: ['measure'] });
 
-const workerFunction = (array) => {
-	return new Promise((resolve, reject) => {
-		performance.mark('worker start');
-		const worker = new Worker('./worker.js', {
-			workerData: {
-				array,
-				file
-			}
-		});
-		worker.on('message', (msg) => {
-			performance.mark('worker end');
-			performance.measure('worker', 'worker start', 'worker end');
+const result = await Promise.all([timedWorker(), timedFork()]);
 
-			resolve(msg);
-		});
-	});
-};
-
-const forkFunction = (array) => {
-	return new Promise((resolve, reject) => {
-		performance.mark('fork start');
-		const forkProcess = fork('./fork.js');
-		forkProcess.send({ array, file });
-		forkProcess.on('message', (msg) => {
-			performance.mark('fork end');
-			performance.measure('fork', 'fork start', 'fork end');
-			resolve(msg);
-		});
-
-	});
-};
-
-const main = async () => {
-	try {
-		await workerFunction([25, 20, 19, 48, 30, 50]);
-		await forkFunction([25, 20, 19, 48, 30, 50]);
-	} catch (e) {
-		console.error(e.message);
-	}
-};
-
-main();
+// console.log(result);
